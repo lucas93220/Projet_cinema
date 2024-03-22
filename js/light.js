66c52c093928a6b234ce92c1e05a5a35b4e143da
lucas93220 <91134554+lucas93220@users.noreply.github.com>
2024-03-22 15:00:42 +0100

    light mode

--- js/light.js
+++ js/light.js
@@ -1,28 +1,31 @@
+const switchThemeBtn = document.querySelector('.ChangeTheme');
+let isDarkMode = localStorage.getItem('isDarkMode') === 'true';
 
-    const switchThemeBtn = document.querySelector('.ChangeTheme');
-    let toggleTheme = 0;
 
-    switchThemeBtn.addEventListener('click',() => {
-        if(toggleTheme == 0){
-            document.documentElement.style.setProperty('--ecritureNav','#fff');
-            document.documentElement.style.setProperty('--background','#282626');
-            document.documentElement.style.setProperty('--backgroundHtml','rgb(35, 34, 34)');
-            document.documentElement.style.setProperty('--backgroundDiv','#696565');
-            document.documentElement.style.setProperty('--backgroundBlack','black');
-            document.documentElement.style.setProperty('--ecritureRouge','#c70101');
-            toggleTheme++;
-            }
+function setTheme() {
+    if (isDarkMode) {
 
-            else{
+        document.documentElement.style.setProperty('--ecritureNav', '#fff');
+        document.documentElement.style.setProperty('--background', '#282626');
+        document.documentElement.style.setProperty('--backgroundHtml', 'rgb(35, 34, 34)');
+        document.documentElement.style.setProperty('--backgroundDiv', '#696565');
+        document.documentElement.style.setProperty('--backgroundBlack', 'black');
+        document.documentElement.style.setProperty('--ecritureRouge', '#c70101');
+    } else {
 
-            document.documentElement.style.setProperty('--ecritureNav','black');
-            document.documentElement.style.setProperty('--background','#white');
-            document.documentElement.style.setProperty('--backgroundHtml','white');
-            document.documentElement.style.setProperty('--backgroundDiv','#001d35');
-            document.documentElement.style.setProperty('--backgroundBlack','white');
-            document.documentElement.style.setProperty('--ecritureRouge','#c70101');
-                toggleTheme--;
+        document.documentElement.style.setProperty('--ecritureNav', 'black');
+        document.documentElement.style.setProperty('--background', 'white');
+        document.documentElement.style.setProperty('--backgroundHtml', 'white');
+        document.documentElement.style.setProperty('--backgroundDiv', '#001d35');
+        document.documentElement.style.setProperty('--backgroundBlack', 'white');
+        document.documentElement.style.setProperty('--ecritureRouge', '#c70101');
+    }
+}
 
-            }
+setTheme();
 
-    })
+switchThemeBtn.addEventListener('click', () => {
+    isDarkMode = !isDarkMode; 
+    localStorage.setItem('isDarkMode', isDarkMode); 
+    setTheme();
+});
